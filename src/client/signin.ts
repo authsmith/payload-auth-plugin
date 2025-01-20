33c299338d9ac830acd3a839719c59da84ad562c
Tony Jimenez <anthony.jimenezb@gmail.com>
2025-01-20 14:34:09 -0400

    bugfix: signin erroring on server actions

--- src/client/signin.ts
+++ src/client/signin.ts
@@ -1,4 +1,5 @@
 import { init } from "./passkey/index.js"
+import { redirect } from "next/navigation"
 
 type Provider = "google" | "github" | "passkey"
 
@@ -6,8 +7,14 @@ export function signin(provider: Provider) {
   if (provider === "passkey") {
     init()
   } else {
+    const redirectURL = "/api/admin/oauth/authorization/" + provider
+    if (!document) {
+      redirect(redirectURL)
+      return
+    }
+
     const link = document.createElement("a")
-    link.href = "/api/admin/oauth/authorization/" + provider
+    link.href = redirectURL
     link.click()
   }
 }
