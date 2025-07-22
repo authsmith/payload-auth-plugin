d0f538709728006e9d549efceb17bae58aa9ab21
bytemuck <github.com@bytemuck.com>
2025-07-22 13:48:19 -0400

    chore: Added Microsoft example

--- examples/with-website/src/components/AfterLogin/index.tsx
+++ examples/with-website/src/components/AfterLogin/index.tsx
@@ -1,18 +1,18 @@
 'use client'
-import React from 'react'
-import { Button } from '@payloadcms/ui'
+
 import './styles.scss'
+import React from 'react'
+
+import { Button } from '@payloadcms/ui'
 import { adminAuthClient } from '@/lib/auth'
 
 export const AdminLogin = () => {
   const { oauth } = adminAuthClient.signin()
 
-  const handleGoogleSignin = async () => {
-    oauth('google')
-  }
-  const handleAuth0Signin = async () => {
-    oauth('auth0')
-  }
+  const handleGoogleSignin = async () => oauth('google')
+  const handleAuth0Signin = async () => oauth('auth0')
+  const handleMicrosoftSignin = async () => oauth('msft-entra')
+
   return (
     <div className="oauth-container">
       <Button type="button" onClick={handleGoogleSignin} className="oauth-btn">
@@ -20,6 +20,9 @@ export const AdminLogin = () => {
       </Button>
       <Button type="button" onClick={handleAuth0Signin} className="oauth-btn">
         Signin with Auth0
+      </Button>
+      <Button type="button" onClick={handleMicrosoftSignin} className="oauth-btn">
+        Signin with Microsoft
       </Button>
     </div>
   )
