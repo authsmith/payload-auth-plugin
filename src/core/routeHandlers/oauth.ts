de506e54d651c9ba177bee7d53641ca2b0099ece
Dalibor Belic <dalibor@kulturniautomat.com>
2025-08-13 21:59:29 +0200

    feat(oauth): add additionalScope parameter to OAuthHandlers

--- src/core/routeHandlers/oauth.ts
+++ src/core/routeHandlers/oauth.ts
@@ -82,6 +82,7 @@ export function OAuthHandlers(
             secret,
             successRedirectPath,
             errorRedirectPath,
+            additionalScope,
           )
         }
         default:
